f6cb9907aa6f734ffba580430130066fb896e274
Aleksandersie <aleksander.sie@gmail.com>
2023-03-06 23:56:18 +0300

    modal window children added

--- src/components/MainWindow/MainWindowHeaderCreateOrderButton.tsx
+++ src/components/MainWindow/MainWindowHeaderCreateOrderButton.tsx
@@ -1,13 +1,14 @@
 import React, {useState} from 'react';
 import styles from "./mainWindowStyles.module.scss"
 import Modal from "../Modal/Modal";
+import CreationOrderWindow from "../CreationOrderWindow/CreationOrderWindow";
 const MainWindowHeaderCreateOrderButton = () => {
     const [modalActive, setModalActive] = useState(false)
     return (
         <>
             <button className={styles.createOrderButton} onClick={()=>setModalActive(true)}>Создать заказ</button>
             <Modal active={modalActive} setActive={setModalActive} >
-
+               <CreationOrderWindow/>
             </Modal>
         </>
 
