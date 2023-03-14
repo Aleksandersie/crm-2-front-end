e3132d379740e960257167f189e5527c0dad7135
AleksanderSav <fardmail@rambler.ru>
2023-03-14 18:59:14 +0300

    selector

--- src/components/OrderCreationWindow/OrderItemWindow.tsx
+++ src/components/OrderCreationWindow/OrderItemWindow.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import styles from "./orderItemStyles.module.scss"
+import Modal from "../Modal/Modal";
+import Selector from "../Selector/Selector";
 const OrderItemWindow = () => {
+    const [categoryActive, setCategoryActive] = React.useState<boolean>(false)
+
     return (
         <div className={styles.orderWindowBody}>
             <div className={styles.wrapper}>
                 <div className={styles.blockWrapper}>
                     <p>Категория</p>
-                    <div className={styles.name}>Выберите категорию</div>
+                    <div className={styles.name} onClick={()=>setCategoryActive(true)}>Выберите категорию</div>
+                    <Modal active={categoryActive} setActive={setCategoryActive} children={123}/>
                 </div>
                 <div className={styles.blockWrapper}>
                     <p>Введите размеры</p>
-                    <div className={styles.name}>Введите размеры</div>
+                   <Selector/>
                 </div>
             </div>
         </div>
---
+++ src/components/Selector/Selector.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Selector:React.FC = () => {
+    return (
+        <select >
+            <co
+            <option>123</option>
+            <option>321</option>
+        </select>
+    );
+};
+
+export default Selector;
