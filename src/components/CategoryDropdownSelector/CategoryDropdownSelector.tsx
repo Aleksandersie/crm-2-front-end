9feb6f7c232935eb1356d68574d1d90dd614520c
AleksanderSav <fardmail@rambler.ru>
2023-03-17 15:32:19 +0300

    eslint

--- src/components/CategoryDropdownSelector/CategoryDropdownSelector.tsx
+++ src/components/CategoryDropdownSelector/CategoryDropdownSelector.tsx
@@ -1,50 +1,75 @@
-import React, {useEffect, useState} from 'react';
-import styles from "./CategoryDropdownSelector.module.scss"
-import {useAppDispatch, useAppSelector} from "../../store/reduxHooks/reduxHooks";
-import {ICategoryItem} from "../../store/slice/orderCategorySlice";
-import {selectedOrderCategorySlice} from "../../store/slice/selectedOrderCategorySlice.";
-import {materialSlice} from "../../store/slice/materiallSlice";
-import {digitalPrintMaterials} from "../../store/slice/materialLists/digitalPrintMaterials";
-import {interiorPrintMaterialList} from "../../store/slice/materialLists/interiorPrintMaterialList";
-import {selectedMaterialSlice} from "../../store/slice/selectedMaterialSlice";
+import React, { useEffect, useState } from "react";
+import styles from "./CategoryDropdownSelector.module.scss";
+import {
+    useAppDispatch,
+    useAppSelector,
+} from "../../store/reduxHooks/reduxHooks";
+import { ICategoryItem } from "../../store/slice/orderCategorySlice";
+import { selectedOrderCategorySlice } from "../../store/slice/selectedOrderCategorySlice.";
+import { materialSlice } from "../../store/slice/materiallSlice";
+import { digitalPrintMaterials } from "../../store/slice/materialLists/digitalPrintMaterials";
+import { interiorPrintMaterialList } from "../../store/slice/materialLists/interiorPrintMaterialList";
+import { selectedMaterialSlice } from "../../store/slice/selectedMaterialSlice";
 
+const CategoryDropdownSelector: React.FC = () => {
+    const dispatch = useAppDispatch();
 
-const CategoryDropdownSelector:React.FC = () => {
-    
-    const dispatch = useAppDispatch()
+    const [activeDropdown, setActiveDropdown] = useState<boolean>(false);
 
-    const [activeDropdown,setActiveDropdown] = useState<boolean>(false)
-
-    function categoryHandler(category:ICategoryItem){
-        setActiveDropdown(false)
-        dispatch(selectedOrderCategorySlice.actions.setSelectedOrderCategory(category.name))
+    function categoryHandler(category: ICategoryItem) {
+        setActiveDropdown(false);
+        dispatch(
+            selectedOrderCategorySlice.actions.setSelectedOrderCategory(
+                category.name
+            )
+        );
     }
 
-    const {name} = useAppSelector(state => state.selectedOrderCategoryReducer)
-    const categories = useAppSelector(state => state.orderCategoryReducer)
+    const { name } = useAppSelector(
+        (state) => state.selectedOrderCategoryReducer
+    );
+    const categories = useAppSelector((state) => state.orderCategoryReducer);
 
     useEffect(() => {
-        console.log("category is",name)
-        if(name === "Цифровая печать" ) {
-            console.log("digital")
-            dispatch(selectedMaterialSlice.actions.setSelectedMaterial(""))
-            dispatch(materialSlice.actions.setMaterialList(digitalPrintMaterials))
+        console.log("category is", name);
+        if (name === "Цифровая печать") {
+            console.log("digital");
+            dispatch(selectedMaterialSlice.actions.setSelectedMaterial(""));
+            dispatch(
+                materialSlice.actions.setMaterialList(digitalPrintMaterials)
+            );
         }
-        if(name === "Печать с резкой" ) {
-            console.log("intPrint")
-            dispatch(selectedMaterialSlice.actions.setSelectedMaterial(""))
-            dispatch(materialSlice.actions.setMaterialList(interiorPrintMaterialList))
+        if (name === "Печать с резкой") {
+            console.log("intPrint");
+            dispatch(selectedMaterialSlice.actions.setSelectedMaterial(""));
+            dispatch(
+                materialSlice.actions.setMaterialList(interiorPrintMaterialList)
+            );
         }
-    },[categoryHandler]);
+    }, [categoryHandler]);
 
     return (
         <div className={styles.selector}>
-            <div className={styles.selectorHeader}
-            onClick={()=>setActiveDropdown(true)}
-            >{name||"Выберите категорию"}</div>
-            <div className={activeDropdown? styles.selectorBody:styles.selectorBodyInactive}>
-                {categories.map(category=>(
-                   <div className={styles.listItem} onClick={()=>categoryHandler(category)}>{category.name}</div>
+            <div
+                className={styles.selectorHeader}
+                onClick={() => setActiveDropdown(true)}
+            >
+                {name || "Выберите категорию"}
+            </div>
+            <div
+                className={
+                    activeDropdown
+                        ? styles.selectorBody
+                        : styles.selectorBodyInactive
+                }
+            >
+                {categories.map((category) => (
+                    <div
+                        className={styles.listItem}
+                        onClick={() => categoryHandler(category)}
+                    >
+                        {category.name}
+                    </div>
                 ))}
             </div>
         </div>
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,26 +1,25 @@
-import {combineReducers, configureStore} from "@reduxjs/toolkit";
-import {priceApi} from "../RTK/priceApi";
-import orderCategoryReducer from "./slice/orderCategorySlice"
-import selectedOrderCategoryReducer from "./slice/selectedOrderCategorySlice."
-import materialSliceReducer from "./slice/materiallSlice"
-import selectedMaterialReducer from "./slice/selectedMaterialSlice"
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { priceApi } from "../RTK/priceApi";
+import orderCategoryReducer from "./slice/orderCategorySlice";
+import selectedOrderCategoryReducer from "./slice/selectedOrderCategorySlice.";
+import materialSliceReducer from "./slice/materiallSlice";
+import selectedMaterialReducer from "./slice/selectedMaterialSlice";
 
+const rootReducer = combineReducers({
+    orderCategoryReducer,
+    selectedOrderCategoryReducer,
+    selectedMaterialReducer,
+    materialSliceReducer,
+    [priceApi.reducerPath]: priceApi.reducer,
+});
 
-const rootReducer = combineReducers(
-    {
-        orderCategoryReducer,
-        selectedOrderCategoryReducer,
-        selectedMaterialReducer,
-        materialSliceReducer,
-        [priceApi.reducerPath]: priceApi.reducer
-    }
-)
+export const setupStore = () =>
+    configureStore({
+        reducer: rootReducer,
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(priceApi.middleware),
+    });
 
-export const setupStore =()=> configureStore( {
-    reducer: rootReducer,
-    middleware:(getDefaultMiddleware)=>getDefaultMiddleware().concat(priceApi.middleware)
-})
-
-export type RootState = ReturnType<typeof rootReducer>
-export type AppStore = ReturnType<typeof setupStore>
-export type AppDispatch = AppStore["dispatch"]
+export type RootState = ReturnType<typeof rootReducer>;
+export type AppStore = ReturnType<typeof setupStore>;
+export type AppDispatch = AppStore["dispatch"];
