79336759e535ee0cf9851ab02fc983cbe9dd7d5b
Aleksandersie <aleksander.sie@gmail.com>
2023-03-23 18:46:34 +0300

    items window

--- src/components/OrderItemsList/OrderItemsList.tsx
+++ src/components/OrderItemsList/OrderItemsList.tsx
@@ -4,15 +4,25 @@ import styles from "./orderItemsListStyles.module.scss";
 const OrderItemsList = () => {
     return (
         <div>
+            <div className={styles.itemPlaceholder}>
+                <div className={styles.categoryPlaceholder}>категория</div>
+                <div className={styles.materialPlaceholder}>материал</div>
+                <div className={styles.itemElement}>размеры</div>
+                <div className={styles.itemElement}>кол-во</div>
+                <div className={styles.itemElement}>цена за шт</div>
+                <div className={styles.itemElement}>итого</div>
+            </div>
             {orderItemsArray.map((item) => (
                 <div key={item.itemRandomNumber} className={styles.itemWrapper}>
-                    <div className={styles.itemElement}>{item.orderCategory}</div>
-                    <div className={styles.itemElement}>{item.orderMaterial}</div>
-                    <div className={styles.itemElement}>{item.width}x{item.height}</div>
+                    <div className={styles.categoryElement}>{item.orderCategory}</div>
+                    <div className={styles.materialElement}>{item.orderMaterial}</div>
+                    <div className={styles.itemElement}>
+                        {item.width}x{item.height} м.
+                    </div>
                     <div className={styles.itemElement}>{item.pieces} шт</div>
                     <div className={styles.itemElement}>{item.itemOnePscCost} руб</div>
                     <div className={styles.itemElement}>{item.itemTotalCost} руб</div>
-                    </div>
+                </div>
             ))}
         </div>
     );
