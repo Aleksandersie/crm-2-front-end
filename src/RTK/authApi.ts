bfcf58e2e4888ed3ba065eb33b5f108d17e275e8
AleksanderSav <fardmail@rambler.ru>
2023-04-18 18:35:03 +0300

    search

--- src/RTK/authApi.ts
+++ src/RTK/authApi.ts
@@ -1,4 +1,5 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { API_ADDRESS } from "./API_ADDRESS";
 
 interface ILoginRequest {
     userName: string;
@@ -11,7 +12,7 @@ interface IAuthResponse {
 
 export const authApi = createApi({
     reducerPath: "authApi",
-    baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000/api/" }),
+    baseQuery: fetchBaseQuery({ baseUrl: API_ADDRESS }),
     tagTypes: ["auth"],
     endpoints: (builder) => ({
         login: builder.mutation<IAuthResponse, ILoginRequest>({
